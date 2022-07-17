e697394406896c6cb2f29127aca780d1d8e8ea38
khanhvo198 <khanh.votuan@hcmut.edu.vn>
2022-07-17 23:14:02 +0700

    config jwt

--- src/main/java/com/mystic/twitter/controllers/AuthenticationController.java
+++ src/main/java/com/mystic/twitter/controllers/AuthenticationController.java
@@ -1,2 +1,26 @@
-package com.mystic.twitter.controllers;public class AuthenticationController {
+package com.mystic.twitter.controllers;
+
+
+import com.mystic.twitter.dtos.request.AuthenticationRequest;
+import com.mystic.twitter.dtos.response.AuthenticationResponse;
+import com.mystic.twitter.security.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+    JwtService jwtService;
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest request) {
+        String token = jwtService.createToken(request.getEmail(), "USER");
+
+        return ResponseEntity.ok(
+                AuthenticationResponse.builder()
+                        .token(token)
+                        .build()
+        );
+    }
 }
--- src/main/java/com/mystic/twitter/security/JwtService.java
+++ src/main/java/com/mystic/twitter/security/JwtService.java
@@ -1,8 +1,38 @@
 package com.mystic.twitter.security;
 
-public class JwtProvider {
-    
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+
+@RequiredArgsConstructor
+public class JwtService {
+
+    private static final String authenticationHeader = "Authentication";
+
+    private final JwtEncoder jwtEncoder;
+
+    public String createToken(String email, String role) {
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .expiresAt(Instant.now().plusSeconds(36000L))
+                .subject(email)
+                .claim("role", role)
+                .claim("test","test")
+                .build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
 
 
+    public boolean validateToken() {
+        return false;
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        return request.getHeader(authenticationHeader);
+    }
 
 }
