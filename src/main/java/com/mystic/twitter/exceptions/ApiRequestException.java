5a7a263ad10856b494f1a545571eb14f1fbb9872
khanhvo198 <khanh.votuan@hcmut.edu.vn>
2022-07-17 23:17:10 +0700

    api request exception

--- src/main/java/com/mystic/twitter/exceptions/ApiRequestException.java
+++ src/main/java/com/mystic/twitter/exceptions/ApiRequestException.java
@@ -1,2 +1,18 @@
-package com.mystic.twitter.exceptions;public class ApiRequestException {
+package com.mystic.twitter.exceptions;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+
+@Getter
+public class ApiRequestException extends RuntimeException{
+
+    private final HttpStatus status;
+
+    public ApiRequestException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.status = httpStatus;
+    }
+
+
 }
