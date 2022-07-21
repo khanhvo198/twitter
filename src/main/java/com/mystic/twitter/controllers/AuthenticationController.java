0eaf24da4eb0a6a727140b4bbc5bfc2a3ab43537
khanhvo198 <khanh.votuan@hcmut.edu.vn>
2022-07-21 23:23:34 +0700

    login controller

--- src/main/java/com/mystic/twitter/controllers/AuthenticationController.java
+++ src/main/java/com/mystic/twitter/controllers/AuthenticationController.java
@@ -1,25 +1,41 @@
 package com.mystic.twitter.controllers;
 
 
+import com.mystic.twitter.dtos.UserDTO;
 import com.mystic.twitter.dtos.request.AuthenticationRequest;
 import com.mystic.twitter.dtos.response.AuthenticationResponse;
+import com.mystic.twitter.models.User;
 import com.mystic.twitter.security.JwtService;
+import com.mystic.twitter.security.UserDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
-    JwtService jwtService;
+    private final JwtService jwtService;
+    private final UserDetailService userService;
     @PostMapping("/login")
     public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest request) {
-        String token = jwtService.createToken(request.getEmail(), "USER");
+        String token = jwtService.generateToken(request.getEmail(), "USER");
+
+        User user = (User) userService.loadUserByUsername(request.getEmail());
+
+        UserDTO userDTO = new UserDTO(
+                user.getEmail(),
+                user.getFirstName(),
+                user.getLastName()
+        );
 
         return ResponseEntity.ok(
                 AuthenticationResponse.builder()
                         .token(token)
+                        .user(userDTO)
                         .build()
         );
     }
