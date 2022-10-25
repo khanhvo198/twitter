e124b33280c06dc0e4b94e4bba83499edca308b9
khanhvo198 <khanh.votuan@hcmut.edu.vn>
2022-10-25 21:46:11 +0700

    config global cors

--- src/main/java/com/mystic/twitter/configurations/WebMvcConfiguration.java
+++ src/main/java/com/mystic/twitter/configurations/WebMvcConfiguration.java
@@ -1,5 +1,6 @@
 package com.mystic.twitter.configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,8 +10,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**");
-  }
+  final String hostName = "localhost:3000";
+
+  // @Override
+  // public void addCorsMappings(CorsRegistry registry) {
+  // registry.addMapping("/api/v1/**")
+  // .allowedOrigins("http://" + hostName)
+  // .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+  // .exposedHeaders("page-total-count")
+  // .allowedHeaders("*");
+  // }
 }
--- src/main/java/com/mystic/twitter/configurations/WebSecurityConfiguration.java
+++ src/main/java/com/mystic/twitter/configurations/WebSecurityConfiguration.java
@@ -1,5 +1,8 @@
 package com.mystic.twitter.configurations;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -7,12 +10,16 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.session.SessionManagementFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import com.mystic.twitter.repository.UserRepository;
 import com.mystic.twitter.security.JwtFilter;
 
 import lombok.RequiredArgsConstructor;
@@ -22,18 +29,31 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity
 public class WebSecurityConfiguration {
   private final JwtFilter jwtFilter;
-  private final UserRepository userRepository;
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+    configuration.setAllowCredentials(true);
+    configuration.setAllowedHeaders(
+        Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin", "Access-Control-Request-Method",
+            "Access-Control-Request-Headers", "Origin", "Cache-Control", "Content-Type", "Authorization"));
+    configuration.setAllowedMethods(Arrays.asList("DELETE", "GET", "POST", "PATCH", "PUT", "OPTIONS"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
+        .cors(Customizer.withDefaults())
         .csrf().disable()
         .authorizeHttpRequests((authz) -> authz
             .antMatchers("/api/v1/auth/**").permitAll()
             .anyRequest().authenticated())
         .httpBasic(Customizer.withDefaults())
         .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-
     return http.build();
   }
 
